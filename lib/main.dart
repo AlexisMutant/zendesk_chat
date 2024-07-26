fb8e5a9a521f0d630cd714f23f60cdc37cc30af2
Alexis Dominguez <alexis@mutant.software>
2024-07-26 12:22:14 -0600

    Change main code for example code

--- lib/main.dart
+++ lib/main.dart
@@ -1,125 +1,171 @@
 import 'package:flutter/material.dart';
+import 'package:zendesk_messaging/zendesk_messaging.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
+  State<MyApp> createState() => _MyAppState();
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
+class _MyAppState extends State<MyApp> {
+  static const String androidChannelKey = "your android key";
+  static const String iosChannelKey = "your iOS key";
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
+  final List<String> channelMessages = [];
 
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+  bool isLogin = false;
+  int unreadMessageCount = 0;
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+  void initState() {
+    super.initState();
+    // Optional, observe all incoming messages
+    ZendeskMessaging.setMessageHandler((type, arguments) {
+      setState(() {
+        channelMessages.add("$type - args=$arguments");
+      });
     });
   }
 
   @override
+  void dispose() {
+    ZendeskMessaging.invalidate();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+    final message = channelMessages.join("\n");
+
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Zendesk Messaging Example'),
+        ),
+        body: SafeArea(
+          child: Container(
+            padding: const EdgeInsets.all(20),
+            child: ListView(
+              children: [
+                Text(message),
+                const SizedBox(
+                  height: 20,
+                ),
+                ElevatedButton(
+                  onPressed: () => ZendeskMessaging.initialize(
+                    androidChannelKey: androidChannelKey,
+                    iosChannelKey: iosChannelKey,
+                  ),
+                  child: const Text("Initialize"),
+                ),
+                if (isLogin) ...[
+                  ElevatedButton(
+                    onPressed: () => ZendeskMessaging.show(),
+                    child: const Text("Show messaging"),
+                  ),
+                  ElevatedButton(
+                    onPressed: () => _getUnreadMessageCount(),
+                    child: Text('Get unread message count - $unreadMessageCount'),
+                  ),
+                ],
+                ElevatedButton(
+                  onPressed: () => _setTags(),
+                  child: const Text("Add tags"),
+                ),
+                ElevatedButton(
+                  onPressed: () => _clearTags(),
+                  child: const Text("Clear tags"),
+                ),
+                ElevatedButton(
+                  onPressed: () => _login(),
+                  child: const Text("Login"),
+                ),
+                ElevatedButton(
+                  onPressed: () => _logout(),
+                  child: const Text("Logout"),
+                ),
+                ElevatedButton(
+                  onPressed: () => _checkUserLoggedIn(),
+                  child: const Text("Check LoggedIn"),
+                ),
+                ElevatedButton(
+                  onPressed: () => _setFields(),
+                  child: const Text("Add Fields"),
+                ),
+                ElevatedButton(
+                  onPressed: () => _clearFields(),
+                  child: const Text("Clear Fields"),
+                ),
+                ElevatedButton(
+                  onPressed: () => _show(),
+                  child: const Text("Show"),
+                ),
+              ],
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+          ),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
+  }
+
+  void _login() {
+    // You can attach local observer when calling some methods to be notified when ready
+    ZendeskMessaging.loginUserCallbacks(
+      jwt: "my_jwt",
+      onSuccess: (id, externalId) => setState(() {
+        channelMessages.add("Login observer - SUCCESS: $id, $externalId");
+        isLogin = true;
+      }),
+      onFailure: () => setState(() {
+        channelMessages.add("Login observer - FAILURE!");
+        isLogin = false;
+      }),
+    );
+  }
+
+  void _logout() {
+    ZendeskMessaging.logoutUser();
+    setState(() {
+      isLogin = false;
+    });
+  }
+  void _getUnreadMessageCount() async {
+    final messageCount = await ZendeskMessaging.getUnreadMessageCount();
+    if (mounted) {
+      unreadMessageCount = messageCount;
+      setState(() {});
+    }
+  }
+  void _setTags() async {
+    final tags = ['tag1', 'tag2', 'tag3'];
+    await ZendeskMessaging.setConversationTags(tags);
+  }
+  void _clearTags() async {
+    await ZendeskMessaging.clearConversationTags();
+  }
+  void _checkUserLoggedIn()async {
+    final isLoggedIn = await ZendeskMessaging.isLoggedIn();
+    setState(() {
+      channelMessages.add('User is ${isLoggedIn?'':'not'} logged in');
+    });
+  }
+  void _setFields() async {
+    Map<String, String> fieldsMap = {};
+
+    fieldsMap["field1"] = "Value 1";
+    fieldsMap["field2"] = "Value 2";
+
+    await ZendeskMessaging.setConversationFields(fieldsMap);
+  }
+  void _clearFields() async {
+    await ZendeskMessaging.clearConversationFields();
+  }
+  void _show() {
+    ZendeskMessaging.show();
   }
 }
